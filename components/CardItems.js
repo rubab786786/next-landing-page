882fc76fd3e6b4f8eaab518aab5d9b48e535f5f9
rubab768768 <rubabmubarikali786@gmail.com>
2024-12-17 12:55:16 +0500

    mobile-responsive

--- components/CardItems.js
+++ components/CardItems.js
@@ -5,7 +5,7 @@ const CardItems = ({heading,img}) => {
         <>
          {/* <div className="industries_slide w-slide">
              <div className="lop"> */}
-                <h1 className="heading-6">{heading}</h1>
+                <h1 className="heading-6" style={{ textAlign: "left" }}>{heading}</h1>
                 <img loading="lazy" src={img} alt="" className="image-13" />
              {/* </div>
          </div> */}
